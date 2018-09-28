9efb1a3817b4788a1a044690f07b9919fbc51640
Nicolas Mattiocco <nicolas@greenlock.fr>
2018-09-28 17:40:44 +0200

    test

--- engines/utils/PatrowlEngineTest.py
+++ engines/utils/PatrowlEngineTest.py
@@ -8,49 +8,16 @@ class PatrowlEngineTest:
         self.engine_name = engine_name
         self.base_url = base_url
 
-    def custom_test(self, test_name, assets, scan_policy={}, is_valid=True, max_timeout=300):
-        print("test-{}-custom: {}".format(self.engine_name, test_name))
-        TEST_SCAN_ID = random.randint(1000000, 1999999)
-        post_data = {
-            "assets":  assets,
-            "options": scan_policy,
-            "scan_id": str(TEST_SCAN_ID)
-        }
-
-        r = requests.post(url="{}/startscan".format(self.base_url),
-                   data=json.dumps(post_data),
-                   headers = {'Content-type': 'application/json', 'Accept': 'application/json'})
+    def test_connectivity(self):
+        print("test-{}-connectivity".format(self.engine_name))
         try:
-            assert r.json()['status'] == "accepted"
+            r = requests.get(url="{}/".format(self.base_url))
+            assert r.status_code == 200
+            assert r.json()["page"] == "index"
+
         except AssertionError:
             print(r.json()) ; assert False
 
-        # Wait until scan is finished
-        timeout_start = time.time()
-        has_error = False
-        while time.time() < timeout_start + max_timeout:
-            r = requests.get(url="{}/status/{}".format(self.base_url, TEST_SCAN_ID))
-            if r.json()["status"] == "SCANNING": continue
-            elif r.json()["status"] == "FINISHED": break
-            elif r.json()["status"] == "ERROR":
-                has_error = True
-                break
-            time.sleep(3)
-
-        # Get findings
-        if not has_error:
-            r = requests.get(url="{}/getfindings/{}".format(self.base_url, TEST_SCAN_ID))
-            try:
-                assert r.json()['status'] == "success"
-            except AssertionError:
-                print(r.json()) ; assert False
-
-            # Get report
-            r = requests.get(url="{}/getreport/{}".format(self.base_url, TEST_SCAN_ID))
-            try:
-                assert r.json()['scan']['status'] == "FINISHED"
-            except AssertionError:
-                print(r.json()) ; assert False
 
     def test_status(self):
         print("test-{}-status".format(self.engine_name))
@@ -97,8 +64,52 @@ class PatrowlEngineTest:
         except AssertionError:
             print(r.json()) ; assert False
 
+    def custom_test(self, test_name, assets, scan_policy={}, is_valid=True, max_timeout=300):
+        print("test-{}-custom: {}".format(self.engine_name, test_name))
+        TEST_SCAN_ID = random.randint(1000000, 1999999)
+        post_data = {
+            "assets":  assets,
+            "options": scan_policy,
+            "scan_id": str(TEST_SCAN_ID)
+        }
+
+        r = requests.post(url="{}/startscan".format(self.base_url),
+                   data=json.dumps(post_data),
+                   headers = {'Content-type': 'application/json', 'Accept': 'application/json'})
+        try:
+            assert r.json()['status'] == "accepted"
+        except AssertionError:
+            print(r.json()) ; assert False
+
+        # Wait until scan is finished
+        timeout_start = time.time()
+        has_error = False
+        while time.time() < timeout_start + max_timeout:
+            r = requests.get(url="{}/status/{}".format(self.base_url, TEST_SCAN_ID))
+            if r.json()["status"] == "SCANNING": continue
+            elif r.json()["status"] == "FINISHED": break
+            elif r.json()["status"] == "ERROR":
+                has_error = True
+                break
+            time.sleep(3)
+
+        # Get findings
+        if not has_error:
+            r = requests.get(url="{}/getfindings/{}".format(self.base_url, TEST_SCAN_ID))
+            try:
+                assert r.json()['status'] == "success"
+            except AssertionError:
+                print(r.json()) ; assert False
+
+            # Get report
+            r = requests.get(url="{}/getreport/{}".format(self.base_url, TEST_SCAN_ID))
+            try:
+                assert r.json()['scan']['status'] == "FINISHED"
+            except AssertionError:
+                print(r.json()) ; assert False
 
     def do_generic_tests(self):
+        self.test_connectivity()
         self.test_status()
         self.test_info()
         self.test_reloadconfig()
