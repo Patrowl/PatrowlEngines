8611b1fd5062ee8c03ab991b42cf7bf041bbe88d
Nicolas Mattiocco <nicolas@greenlock.fr>
2018-10-02 00:30:28 +0200

    test

--- engines/owl_leaks/engine-owl_leaks.py
+++ engines/owl_leaks/engine-owl_leaks.py
@@ -51,7 +51,7 @@ def index(): return engine.index()
 @app.route('/engines/owl_leaks/test')
 def test(): return engine.test()
 
-@app.route('/engines/owl_code/reloadconfig')
+@app.route('/engines/owl_leaks/reloadconfig')
 def reloadconfig(): return engine.reloadconfig()
 
 @app.route('/engines/owl_leaks/info')
