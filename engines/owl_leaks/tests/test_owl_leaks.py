3d1aed4f7a833f04b11c4021b78d439c7dc75b63
Nicolas Mattiocco <nicolas@greenlock.fr>
2018-10-01 23:15:02 +0200

    test

--- engines/owl_leaks/tests/test_owl_leaks.py
+++ engines/owl_leaks/tests/test_owl_leaks.py
@@ -13,21 +13,34 @@ def test_generic_features():
     pet.do_generic_tests()
 
 ## custom tests
-# def test_owlleaks_subdomain_enum():
-#     pet.custom_test(
-#         test_name="owldns_subdomain_enum",
-#         assets=[{
-#             "id" :'1',
-#             "value" :'patrowl.io',
-#             "criticity": 'low',
-#             "datatype": 'domain'
-#         }],
-#         scan_policy={
-#             ...
-#         },
-#         is_valid=True
-#     )
+def test_owlleaks_patrowl():
+    pet.custom_test(
+        test_name="owlleaks_patrowl",
+        assets=[{
+            "id" :'1',
+            "value" :'patrowl.io',
+            "criticity": 'low',
+            "datatype": 'keyword'
+        }],
+        scan_policy={
+            "max_timeout": 3600,
+            "search_github": False,
+            # "search_github": True,
+            "github_qualifiers": {
+                # "since_period": "30days",
+                "from_date": "2010-01-01"
+            },
+            # "search_twitter": False,
+            "search_twitter": True,
+            "search_twitter_options": {
+                "max_count": 10,
+                "extra_kw": ["hack", "attack", "leak", "fuite", "phishing", "ddos"],
+                "since": "2017-01-01"
+            }
+        },
+        is_valid=True
+    )
 
 if __name__ == '__main__':
     test_generic_features()
-    #test_owlleaks_subdomain_enum()
+    test_owlleaks_patrowl()
