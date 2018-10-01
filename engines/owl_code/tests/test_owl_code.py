5729a3ad4dcd81160d1035e98ee21a3e51546626
Nicolas Mattiocco <nicolas@greenlock.fr>
2018-10-01 23:11:24 +0200

    test

---
+++ engines/owl_code/tests/test_owl_code.py
@@ -0,0 +1,33 @@
+import sys, os
+import json, requests, time, random
+
+sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
+from utils.PatrowlEngineTest import PatrowlEngineTest
+
+# Define the engine instance
+pet = PatrowlEngineTest(engine_name="owl_code", base_url="http://127.0.0.1:5013/engines/owl_code")
+MAX_TIMEOUT = 300   # in seconds
+
+# generic tests
+def test_generic_features():
+    pet.do_generic_tests()
+
+## custom tests
+# def test_owlleaks_subdomain_enum():
+#     pet.custom_test(
+#         test_name="owldns_subdomain_enum",
+#         assets=[{
+#             "id" :'1',
+#             "value" :'patrowl.io',
+#             "criticity": 'low',
+#             "datatype": 'domain'
+#         }],
+#         scan_policy={
+#             ...
+#         },
+#         is_valid=True
+#     )
+
+if __name__ == '__main__':
+    test_generic_features()
+    #test_owlleaks_subdomain_enum()
