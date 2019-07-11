42e76f4c2b3b69be057b5a491f80febd6eb711a1
Alexandre Derumier <aderumier@odiso.com>
2019-07-11 12:32:39 +0200

    docker : displays logs to stdout

--- scripts/start-docker-engines.sh
+++ scripts/start-docker-engines.sh
@@ -9,6 +9,6 @@ for engine_dir in $engines_path/*; do
     # get the default port
     engine_port=$(grep "APP_PORT = " engine-${engine_dir##*/}.py | cut -d'=' -f2 | sed 's/^ *//g')
     docker stop ${engine_dir##*/}-docker-001
-    docker run -d --rm -p $engine_port:$engine_port --name="${engine_dir##*/}-docker-001" -v $engines_path/${engine_dir##*/}/${engine_dir##*/}.json:/opt/patrowl-engines/${engine_dir##*/}/${engine_dir##*/}.json:ro patrowl-${engine_dir##*/}
+    docker run -t -d --rm -p $engine_port:$engine_port --name="${engine_dir##*/}-docker-001" -v $engines_path/${engine_dir##*/}/${engine_dir##*/}.json:/opt/patrowl-engines/${engine_dir##*/}/${engine_dir##*/}.json:ro patrowl-${engine_dir##*/}
     fi
 done
