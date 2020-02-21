b4590f549974ec7eddd37d781af88e94308d89a5
Harduino <mail@harduino.com>
2020-02-21 17:33:21 +0300

    openvas :: force to use urllib.parse due to python3 is used anyway

--- engines/openvas/engine-openvas.py
+++ engines/openvas/engine-openvas.py
@@ -10,13 +10,9 @@ from re import search as re_search
 from subprocess import check_output
 from threading import Thread
 from time import time, sleep
+import urllib.parse as urlparse
 from uuid import UUID
 import xml.etree.ElementTree as ET
-
-try:
-    import urlparse
-except ImportError:
-    import urllib.parse as urlparse
 
 # Third party library imports
 from flask import Flask, request, jsonify
