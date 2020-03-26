8b112d0b08df76f1801a5bf70220a103d75acd54
Sebastien <sebastien.coll@patrowl.io>
2020-03-26 17:20:39 +0100

    [DROOPESCAN] Add travis CI tests

--- engines/droopescan/tests/test_droopescan.py
+++ engines/droopescan/tests/test_droopescan.py
@@ -12,40 +12,24 @@ BASE_URL = "http://127.0.0.1:5021/engines/droopescan"
 # Define the engine instance
 PET = PatrowlEngineTest(engine_name="droopescan", base_url=BASE_URL)
 
-
 def test_generic_features():
     """Generic tests."""
     PET.do_generic_tests()
 
-# TODO: Fix "No PID found" error
-# def test_nmap_vulners():
-#     """Custom tests."""
-#     PET.custom_test(
-#         test_name="nmap_vulners",
-#         assets=[{
-#             "id": "1",
-#             "value": "patrowl.io",
-#             "criticity": "low",
-#             "datatype": "domain"
-#         }],
-#         scan_policy={
-#             "no_ping": 0,
-#             "ports": [
-#                 "80",
-#                 "22",
-#                 "443",
-#                 "56",
-#                 "25"
-#             ],
-#             "detect_service_version": 1,
-#             "script": "libs/vulners.nse",
-#             "show_open_ports": 1
-#         },
-#         is_valid=True
-#     )
-
+def test_droopescan_check_website(website,fqdn):
+    """ custom tests """
+    PET.custom_test(
+        test_name="ssllabs_check_"+website,
+        assets=[{
+            "id": "1",
+            "value": "https://"+fqdn,
+            "criticity": "low",
+            "datatype": "url"
+        }],
+        scan_policy={
+            "scan_wordpress": 1,
+        })
 
 if __name__ == "__main__":
     test_generic_features()
-    # TODO: Fix "No PID found" error
-    # test_nmap_vulners()
+    test_droopescan_check_website("novepha", "www.novepha.fr")
