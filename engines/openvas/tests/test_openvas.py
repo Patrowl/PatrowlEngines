6198af2e13c4f353876319457d8f9a97c20510e8
Sébastien COLL <sebastien.coll@patrowl.io>
2020-11-26 15:30:35 +0100

    OPENVAS Updated tests

--- engines/openvas/tests/test_openvas.py
+++ engines/openvas/tests/test_openvas.py
@@ -18,55 +18,6 @@ def test_generic_features():
     """Generic tests."""
     PET.do_generic_tests()
 
-
-def test_openvas_default_scan_ip():
-    """Scan an IP."""
-    PET.custom_test(
-        test_name="openvas_default_scan_ip",
-        assets=[{
-            "id": "1",
-            "value": "8.8.8.8",
-            "criticity": "low",
-            "datatype": "ip"
-        }],
-        scan_policy={
-            "max_timeout": MAX_TIMEOUT,
-            "credential_name": False,
-            "scan_config_name": False,
-            "enable_create_target": False,
-            "enable_create_task": False,
-            "enable_start_task": False
-        },
-        is_valid=True
-    )
-
-
-def test_openvas_default_scan_domain():
-    """Scan a domain."""
-    PET.custom_test(
-        test_name="openvas_default_scan_domain",
-        assets=[{
-            "id": "1",
-            "value": "patrowl.io",
-            "criticity": "low",
-            "datatype": "domain"
-        }],
-        scan_policy={
-            "max_timeout": MAX_TIMEOUT,
-            "credential_name": False,
-            "scan_config_name": False,
-            "enable_create_target": False,
-            "enable_create_task": False,
-            "enable_start_task": False
-        },
-        is_valid=True
-    )
-
-
 if __name__ == "__main__":
     test_generic_features()
-# FIXME Temporarily deactivate these tests, as they failed without
-# any proper openvas instance set up.
-# Add instance creation for travis CI and re-add them
-#    test_openvas_default_scan_ip()
-#    test_openvas_default_scan_domain()
+# FIXME Temporarily remove tests that require an openvas instance
