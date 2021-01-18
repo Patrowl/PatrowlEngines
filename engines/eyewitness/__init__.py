cdf118bc4915f2138a53c1529419bfcf14289e22
Nicolas Béguier <nicolas.beguier@adevinta.com>
2021-01-18 17:18:36 +0100

    1.4.17: Eyewitness timestamp update

--- engines/eyewitness/__init__.py
+++ engines/eyewitness/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'patrowl_engine_eyewitness'
-__version__ = '1.4.15'
+__version__ = '1.4.17'
 __author__ = 'Nicolas Béguier'
 __license__ = 'AGPLv3'
 __copyright__ = 'Copyright (C) 2018-2021 Nicolas Mattiocco - @MaKyOtOx'
