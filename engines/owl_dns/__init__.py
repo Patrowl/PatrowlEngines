72c91d51e220fae14e51542bf2f3722f5aaf4f4d
Sébastien COLL <sebastien.coll@patrowl.io>
2021-09-10 15:19:08 +0200

    OWL_DNS: 1.4.29 Fixed critical bug

--- engines/owl_dns/__init__.py
+++ engines/owl_dns/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'patrowl_engine_owl_dns'
-__version__ = '1.0.0'
+__version__ = '1.4.29'
 __author__ = 'Nicolas MATTIOCCO'
 __license__ = 'AGPLv3'
 __copyright__ = 'Copyright (C) 2018-2019 Nicolas Mattiocco - @MaKyOtOx'
