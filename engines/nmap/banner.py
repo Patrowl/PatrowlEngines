6f6aee3974d171be0b87c591475f0402d8c4f82f
MaKyOtOx <nicolas.mattiocco@patrowl.io>
2022-07-22 11:12:20 +0200

    nmap: clean banner file

--- engines/nmap/banner.py
+++ engines/nmap/banner.py
@@ -1,4 +1,29 @@
-# !/usr/bin/python
+#!/usr/bin/env -S python3 -OO
+# coding:utf8
+
+# Copyright (c) 2021-2022, Patrowl and contributors
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.  Redistributions in binary
+# form must reproduce the above copyright notice, this list of conditions and the
+# following disclaimer in the documentation and/or other materials provided with
+# the distribution
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import socket
 import ssl
 
@@ -97,21 +122,21 @@ def grab_https_banner(target: str, port: int):
 
 def grab_banner(host: str, port: int) -> str:
     """Grab banner from remote service."""
-    # print("Check TCP/", port)
+    # Check generic TCP service
     resp, err = grab_tcp_banner(host, port)
     if err is False and resp not in [None, '']:
         return resp
 
+    # Try checking HTTP service
     if err is True and resp == '':
-        # print("Check HTTP/", port)
         resp, err = grab_http_banner(host, port)
 
+    # Try checking https service instead
     https_required = [
         'HTTPS is required',
         'The plain HTTP request was sent to HTTPS'
     ]
     if any(x in resp for x in https_required):
-        # print("Check HTTPS/", port)
         resp, err = grab_https_banner(host, port)
 
     return resp
@@ -119,7 +144,7 @@ def grab_banner(host: str, port: int) -> str:
 
 def main():
     ports = [22, 80, 443]
-    host = 'patrowl.io'
+    host = 'xxx'
     for port in ports:
         resp = grab_banner(host, port)
         print(resp)
