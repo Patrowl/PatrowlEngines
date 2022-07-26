d60c57328608466a156698b051ba077d60b3c08d
rajporus19 <raj.porus.hiruthayaraj@gmail.com>
2022-07-26 14:32:53 +0200

    script for testing docker slim

---
+++ USEDOCKERSLIM.md
@@ -0,0 +1,9 @@
+To use docker-slim, install it following the instructions at : https://github.com/docker-slim/docker-slim
+then edit the engines_list variable inside docker-slim.sh file to have only the engines  you want.
+then run it :
+- it will build the engine
+- run it
+- docker-slim to probe the running container
+- create the slim image
+- remove the running container
+- run a container from the slim image
---
+++ docker-slim.sh
@@ -0,0 +1,52 @@
+get_engine_port() {
+    engine=$1
+    port=$(grep "EXPOSE 5" engines/$engine/Dockerfile | awk  '{print $2}')
+    echo $port
+}
+
+docker_build_and_run() {
+    engine=$1
+    port=$(get_engine_port $engine)
+    docker build -t $engine -f engines/$engine/Dockerfile engines/$engine/
+    docker run -d -p $port:$port $engine
+    docker-slim build --http-probe=false --exec "python3 tests/*" $engine
+    remove_container_from_image $engine
+    slim_engine="${engine}.slim"
+    docker run -d -p $port:$port $slim_engine
+}
+
+remove_container_from_image() {
+    docker ps -a | awk '{ print $1,$2 }' | grep $1 | awk '{print $1 }' | xargs -I {} docker rm -f {}
+}
+
+
+engine_list="apivoid
+arachni
+certstream
+cortex
+cybelangel
+droopescan
+eyewitness
+nessus
+nmap
+openvas
+owl_code
+owl_dns
+owl_leaks
+owl_request
+pastebin_monitor
+shhgit
+ssllabs
+sslscan
+urlvoid
+virustotal
+wpscan"
+
+engine_list="apivoid
+owl_dns"
+
+for i in $test; 
+    do docker_build_and_run $i;
+done
+
+
