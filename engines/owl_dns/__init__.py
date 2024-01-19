dd62b91ca7277812650fe3cb55b68cb2b9f01d2c
sebastien <sebastien@seb-debian.patrowl.io>
2024-01-19 11:02:18 +0100

    Updated VERSION (owl_dns)

--- engines/owl_dns/__init__.py
+++ engines/owl_dns/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = "patrowl_engine_owl_dns"
-__version__ = "1.5.7-rc2"
+__version__ = "1.5.7"
 __author__ = "Nicolas MATTIOCCO"
 __license__ = "AGPLv3"
 __copyright__ = "Copyright (C) 2018-2024 Nicolas Mattiocco - @MaKyOtOx"
