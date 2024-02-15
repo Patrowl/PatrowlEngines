33d1d9fa9bb53dbf2695d42e9306906a3f6e207d
sebastien <sebastien@seb-debian.patrowl.io>
2024-02-15 17:52:02 +0100

    Black format & skeleton update ARS-319

--- engines/wpscan/__init__.py
+++ engines/wpscan/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'patrowl_engine_wpscan'
-__version__ = '1.4.28'
+__version__ = '1.4.29'
 __author__ = 'Nicolas Béguier'
 __license__ = 'AGPLv3'
 __copyright__ = 'Copyright (C) 2020-2021 Nicolas Mattiocco - @MaKyOtOx'
